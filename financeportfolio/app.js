e7f7dc1eca68ddb510544b8c9449272dbdbbe3c1
Purval <purval1989@gmail.com>
2015-04-21 22:16:13 -0700

    update app.js

--- financeportfolio/app.js
+++ financeportfolio/app.js
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , yahooFinance = require('yahoo-finance')
-  , http = require('http')
-  , path = require('path')
-  , finance = require('./routes/finance');
-var app = express();
-
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-//app.get('/', routes.index);
-app.get('/', function(req,res){
-	yahooFinance.historical({
-	  symbol: 'AAPL',
-	  from: '2015-04-01',
-	  to: '2015-04-15' 
-	}, function (err, quotes) {
-		console.log(quotes);
-	});
-});
-
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-app.get('/quotes/stocksymbol/:stocksymbol',finance.getQuotes);
-app.get('/quotes/monthlyQuotes/:stocksymbol' ,finance.getMonthlyQuotes);
-app.get('/quotes/dailyQuotes/:stocksymbol' ,finance.getDailyQuotes);
-
-
-=======
->>>>>>> origin/master
 /**
  * Module dependencies.
  */
@@ -114,7 +58,3 @@ wsServer.on('request', function(request) {
     connection.on('close', function(connection) {     
     });
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
