ff327f1e2ec61ddcb3bff02da7ccb7f8d25a8d64
purval <purval1989@gmail.com>
2015-04-29 19:35:19 -0700

    mysql dbconnection

--- financeportfolio/dbconnectivity/dbConnection.js
+++ financeportfolio/dbconnectivity/dbConnection.js
@@ -1,8 +1,50 @@
+var mysql = require('mysql2');
 var redis = require("redis");
 var client = redis.createClient(6379,"127.0.0.1");
 
 function getRedisConnection(){
 	return client;
 }
+
+/*var query = "insert into users "
+	+ "(firstname,lastname,email,password,user_type,lastLoggedIn)"
+	+ "values (?,?,?,?,?,now());";
+pool.getConnection(function(err, connection) {
+connection.query(query, [ firstName, lastName, email, password,userType],
+		function(regerr, rows) {
+
+			if (regerr) {
+				pool.releaseConnection(connection);
+				console.log("Error regiter user : " + regerr);
+			} else {
+				pool.releaseConnection(connection);
+				callback(regerr, rows);
+			}
+		});
+});*/
+
+exports.getPoolInstance = function(){
+	
+	if(pool != null){
+		return pool;
+	}
+	else
+	{
+		pool  = mysql.createPool({
+
+			host     : 'cmpe282lab.cckbiaous4u7.us-west-1.rds.amazonaws.com',
+
+			user     : 'CMPE282',
+
+			password : 'cmpe282shim',
+
+			port : '3306',
+
+			database : 'busi_soc_net'
+
+		});
+		return pool;
+	}	
+};
 
 exports.getRedisConnection = getRedisConnection;
